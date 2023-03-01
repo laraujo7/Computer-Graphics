019ba05936ae6a4953cb9986bf2502aa52b35736
Luis Araújo <l.araujoo.1996@gmail.com>
2023-03-01 21:25:49 +0000

    feat(data): add new way to parse data & add git workflow

---
+++ generator/src/config/global.h
@@ -0,0 +1,10 @@
+#ifndef GLOBAL_CONFIG_H
+#define GLOBAL_CONFIG_H
+
+using namespace std;
+
+string UNSUFFICIENT_ARGUMENT = "\ninsufficient number of arguments. \n\n";
+string OBJECT_FOUND_INSUFFICIENT_ARGUMENTS =
+    "\nObject found, but the number of arguments is insufficient. \n\n";
+
+#endif
