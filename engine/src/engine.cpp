300b73a1cd5cd5118e519226f9a3befef990f0d1
cgsp <carlosgsp39@gmail.com>
2023-03-08 02:11:36 +0000

    Set window size from XML

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -1,4 +1,3 @@
-/*
 #include <GL/freeglut_std.h>
 #include <GL/gl.h>
 #ifdef __APPLE__
@@ -6,9 +5,10 @@
 #else
 #include <GL/glut.h>
 #endif
-*/
+
 
 #include "tinyxml2/tinyxml2.cpp"
+#include "tinyxml2/tinyxml2.h"
 #include <iostream>
 #include <iomanip>
 
@@ -17,12 +17,16 @@ using namespace std;
 
 #include <math.h>
 
+//TODO: make camera an object
+
 float camX = 1.0;
 float camY = 1.0;
 float camZ = 1.0;
 float angle = 0;
 
-/*
+int w;
+int h;
+
 void changeSize(int w, int h) {
 
 	// Prevent a divide by zero, when window is too short
@@ -78,14 +82,6 @@ void renderScene(void) {
 
 	glRotatef(angle, camX, camY, camZ);
 
-
-// put axis drawing in here
-	glBegin(GL_TRIANGLES);
-		glVertex3f(0.0f, 0.0f, 0.0f);
-		glVertex3f(1.0f, 1.0f, 0.0f);
-		glVertex3f(-1.0f, 1.0f, 0.0f);
-	glEnd();
-
 	glutWireTeapot(1);
 
 	
@@ -107,16 +103,16 @@ void keyboardCtrl(unsigned char key, int x, int y){
 
 	switch(key){
 		case 'd': // direita
-			camX -= 1.0;
+			camX -= 50.0;
 			break;
 		case 'a': // esquerda
-			camX += 1.0;
+			camX += 50.0;
 			break;
 		case 'w': // cima
-			camY += 1.0;
+			camZ += 50.0;
 			break;
 		case 's': // baixo
-			camY -= 1.0;
+			camZ -= 50.0;
 			break;
 	}
 
@@ -131,34 +127,93 @@ void mouseCtrl(int button, int state, int x, int y){
 	}
 	glutPostRedisplay();
 }
-*/
 
+void setupCamera(XMLElement* camera){
+	int x, y, z;
+
+	for(; camera != NULL; camera = camera->NextSiblingElement()){
+		
+		string camAttribute = camera->Name();
+		
+		if(camAttribute != "projection"){
+			x = atoi(camera->Attribute("x"));
+			y = atoi(camera->Attribute("y"));
+			z = atoi(camera->Attribute("z"));
+			printf("debug x: %d, y: %d, z: %d \n", x,y,z);
+
+			if(camAttribute == "position"){
+				//setCameraPosition(x,y,z);
+				camX = x;
+				camY = y;
+				camZ = z;
+			}
+			else if(camAttribute == "lookAt"){
+				//setCameraLookAt(x,y,z);
+			}
+			else if(camAttribute == "up"){
+				//setCameraUp(x,y,z);
+			}
+		}
+		else {
+			int fov = atoi(camera->Attribute("fov"));
+			int near = atoi(camera->Attribute("near"));
+			int far = atoi(camera->Attribute("far"));
+			printf("debug fov: %d, near: %d, far: %d \n", fov,near,far);
+			float ratio = w * 1.0 / h;
+			gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
+
+		}
+
+	}
+}
+
+void setupWorld(XMLElement* worldElement){
+	
+	// obtenho o primeiro elemento (no caso <window>)
+	XMLElement *e = worldElement->FirstChildElement();
+
+	// percorrer os "nodos" do ficheiro XML
+    while (e != NULL){
+	
+		// para cada nodo tiro o seu valor (<camera>, <position>, ...)
+        string param = e->Value();
+
+		if(param == "window"){
+			int width = atoi(e->Attribute("width"));
+			int height = atoi(e->Attribute("height"));
+
+			if (width > 0 && height > 0){
+				w = width; h = height;
+				glutReshapeWindow(width,height);
+			}
+		}
+		else if (param == "camera"){
+			
+			XMLElement *camera = e->FirstChildElement();
+			setupCamera(camera);
+			
+		}
+		else if (param == "lights"){
+			// percorrer luzes
+		}
+		else if (param == "group"){
+			// transform
+			// models
+
+		}
+		e = e->NextSiblingElement();
+	}
+
+
+}
 int main(int argc, char **argv) {
 
-    XMLDocument doc;
-    const char * path = "xml_teste.xml";
-    char amountText[100];
-    bool loadOk = doc.LoadFile(path);
-    XMLElement * pRootElement = doc.RootElement();
-    if (loadOk == XML_SUCCESS) {
-        XMLElement * s = doc.FirstChildElement( "world" );
-        
-        //retirar atributos da window
-        //retirar atributos da camera
-            //posição
-            //lookat
-
-    } else {
-        cout << "Erro ao ler ficheiro XML->" << path;
-    }
-
-/*
 // init GLUT and the window
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
 	glutInitWindowPosition(100,100);
 	glutInitWindowSize(800,800);
-	glutCreateWindow("nice c0ck");
+	glutCreateWindow("Teste");
 		
 // Required callback registry 
 	glutDisplayFunc(renderScene);
@@ -173,9 +228,22 @@ int main(int argc, char **argv) {
 //  OpenGL settings
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
+
+//  Reading XML config file and setting up
+
+    XMLDocument doc;
+    const char * path = "xml_teste.xml";
+    bool loadOk = doc.LoadFile(path);
+    XMLElement * worldElement = doc.RootElement();
+    
+	if (loadOk == XML_SUCCESS) {
+        setupWorld(worldElement);
+    } else {
+        cout << "Erro ao ler ficheiro XML->" << path;
+    }
 	
 // enter GLUT's main cycle
 	glutMainLoop();
-	*/
+
 	return 1;
 }
