7e1a89f49458d84e611fa12ca7ff116fa65d925b
cgsp <carlosgsp39@gmail.com>
2023-06-03 17:41:37 +0100

    added box normals

--- generator/src/models/box/box.cpp
+++ generator/src/models/box/box.cpp
@@ -58,6 +58,57 @@ void get_box_points(float length, int divisions, vector<Point> &points) {
   }
 }
 
+void get_box_normals(float length, int divisions, vector<Point> &normals) {
+  for (int face = 0; face < nFace; face++) {
+    for (int i = 0; i <= divisions; i++) {
+      for (int j = 0; j <= divisions; j++) {
+
+        Point normal;
+        switch (face) {
+        case 0:
+          // Bottom
+          normal = Point(0, -1, 0);
+          normals.push_back(normal);
+          break;
+
+        case 1:
+          // Front
+          normal = Point(0, 0, -1);
+          normals.push_back(normal);
+          break;
+
+        case 2:
+          // Left
+          normal = Point(-1, 0, 0);
+          normals.push_back(normal);
+          break;
+
+        case 3:
+          // Top
+          normal = Point(0, 1, 0);
+          normals.push_back(normal);
+          break;
+
+        case 4:
+          // Back
+          normal = Point(0, 0, 1);
+          normals.push_back(normal);
+          break;
+
+        case 5:
+          // Right
+          normal = Point(1, 0, 0);
+          normals.push_back(normal);
+          break;
+
+        default:
+          break;
+        }
+      }
+    }
+  }
+}
+
 void get_box_indexs(float length, int divisions,
                     vector<TriangleIndex> &triangules_indexs) {
 
@@ -85,12 +136,15 @@ void get_box_indexs(float length, int divisions,
 
 int create_box(float length, int divisions, string file_name) {
   vector<Point> points;
+  vector<Point> normals;
   vector<TriangleIndex> triangules_indexs;
 
   get_box_points(length, divisions, points);
 
   get_box_indexs(length, divisions, triangules_indexs);
 
+  get_box_normals(length, divisions, normals);
+  
   Model model(points, triangules_indexs);
   model.write_to_file(file_name, "box");
 
